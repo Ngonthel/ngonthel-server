14a68a9de6a7d01b2b234a81af2f30a68293a492
HuseinHQ <mhusein463@gmail.com>
2023-11-17 10:13:45 +0700

    Update errorHandler in services/app

--- services/app/middlewares/errorHandler.js
+++ services/app/middlewares/errorHandler.js
@@ -3,6 +3,14 @@ const errorHandler = (err, req, res, next) => {
   let status = 500;
   let message = "Internal Server Error!";
 
+  if (err.name === "auth_error") {
+    status = 401;
+    message = err.message;
+  } else if (err.name === "validation_error") {
+    status = 400;
+    message = err.message;
+  }
+
   res.status(status).json({ message });
 };
 
